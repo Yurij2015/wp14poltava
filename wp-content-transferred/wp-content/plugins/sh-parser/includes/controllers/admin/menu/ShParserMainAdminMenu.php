0fedcd4032817175240e5f8e6a5c147621fdea77
Yurij <nabor_2008@i.ua>
2017-02-15 01:30:35 +0200

    menu

---
+++ wp-content-transferred/wp-content/plugins/sh-parser/includes/controllers/admin/menu/ShParserMainAdminMenu.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace includes\controllers\admin\menu;
+
+class ShParserMainAdminMenu extends ShParserBaseAdminMenu
+{
+    public function action()
+    {
+        // TODO: Implement action() method.
+        /**
+         * add_menu_page( $page_title, $menu_title, $capability, $menu_slug, $function, $icon_url, $position );
+         *
+         */
+        $plaginPage = add_menu_page(
+            _x(
+                'ShParser',
+                'admin menu page',
+                SHPARSER_PLUGIN_TEXTDOMAIN
+            ),
+            _x(
+                'ShParser',
+                'admin menu page',
+                SHPARSER_PLUGIN_TEXTDOMAIN
+            ),
+            'manage_options',
+            SHPARSER_PLUGIN_TEXTDOMAIN,
+            array(&$this, 'render'),
+            SHPARSER_PLUGIN_URL . 'assets/images/main-menu.png'
+        );
+    }
+
+    /**
+     * Метод отвечающий за контент страницы
+     */
+    public function render()
+    {
+        // TODO: Implement render() method.
+        _e("Hello world, this is ShParser", SHPARSER_PLUGIN_TEXTDOMAIN);
+    }
+
+    public static function newInstance()
+    {
+        // TODO: Implement newInstance() method.
+        $instance = new self;
+        return $instance;
+    }
+}
