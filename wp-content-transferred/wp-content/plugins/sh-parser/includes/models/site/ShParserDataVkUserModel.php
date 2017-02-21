681e3d3e815ba788df46eeb2b45ded2da6933657
Yurij <nabor_2008@i.ua>
2017-02-21 15:36:40 +0200

    vk api

---
+++ wp-content-transferred/wp-content/plugins/sh-parser/includes/models/site/ShParserDataVkUserModel.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Yurij
+ * Date: 21.02.2017
+ * Time: 14:30
+ */
+namespace includes\models\site;
+
+use includes\common\ShParserRequestApi;
+use includes\controllers\admin\menu\ShParserICreatorinstance;
+
+
+class ShParserDataVkUserModel
+{
+
+}
