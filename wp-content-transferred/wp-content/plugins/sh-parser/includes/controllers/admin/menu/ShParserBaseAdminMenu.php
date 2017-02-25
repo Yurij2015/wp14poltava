5227be55eaa55ff8d0ad75107b10ab2817a0ab07
Yurij <nabor_2008@i.ua>
2017-02-25 06:40:08 +0200

    ShParserDefaultOption

--- wp-content-transferred/wp-content/plugins/sh-parser/includes/controllers/admin/menu/ShParserBaseAdminMenu.php
+++ wp-content-transferred/wp-content/plugins/sh-parser/includes/controllers/admin/menu/ShParserBaseAdminMenu.php
@@ -4,15 +4,43 @@ namespace includes\controllers\admin\menu;
 
 abstract class ShParserBaseAdminMenu
 {
-    public function __construct(){
+    public function __construct()
+    {
         /*
          * Регистрирует хук-событие. При регистрации указывается PHP функция,
          * которая сработает в момент события, которое вызывается с помощью do_action().
          */
-        add_action('admin_menu', array( &$this, 'action'));
+        add_action('admin_menu', array(&$this, 'action'));
     }
 
     abstract public function action();
+
     abstract public function render();
-   // abstract public static function newInstance();
+    // abstract public static function newInstance();
+
+    /**
+     * Метод подключения view
+     * @param $view
+     * @param int $type
+     * @param array $data
+     */
+
+    protected function loadView($view, $type = 0, $data = array())
+    {
+        if (file_exists($view)) {
+            switch ($type) {
+                case 0:
+                    require_once $view;
+                    break;
+                case 1:
+                    require $$view;
+                    break;
+                default:
+                    require_once $view;
+                    break;
+            }
+        } else {
+            wp_die(sprintf(__('(View %s not found)', SHPARSER_PLUGIN_TEXTDOMAIN), $view));
+        }
+    }
 }
